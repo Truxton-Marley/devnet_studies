80eded46e72a2ecf4e9542710ad200f7f4148ad8
Truxton-Marley <stevenstruxtonm@gmail.com>
2020-09-28 22:43:51 +0200

    Add super basic I/O file Python code workshop tutorial

--- python_intermediate/io_basics.py
+++ python_intermediate/io_basics.py
@@ -1,5 +1,26 @@
+import sys
+
 def jogger(forgettable):
-    file = open("database.txt", "w")
+    # r: read, w: clobber and write, a: append, x: fail if file exists
+    file = open("things_to_remember.txt", "a")
+    file.write(f'{forgettable}\n')
+    file.close()
+
+def jogger_v2(forgettable):
+    """Add items that might be forgotten to a list file"""
+    with open("things_to_remember.txt", "a") as file:
+        file.write(f'{forgettable}\n')
+
+def show():
+    """Display items that have been added to the list"""
+    with open("things_to_remember.txt") as file:
+        for line in file:
+            print(line)
 
 if __name__ == "__main__":
-    jogger(input("Don't forget:\n"))
+    if sys.argv[1].lower() == "--list":
+        show()
+    elif sys.argv[1].lower() == "--clear":
+        pass
+    else:
+        jogger(input("Don't forget:\n"))
